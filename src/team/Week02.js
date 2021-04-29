14f917446890d80fdd818e727bcf9161721d9287
Davisa94 <Austin.i.davis94@gmail.com>
2021-04-29 16:32:48 -0700

    Added team project files

---
+++ src/team/Week02.js
@@ -0,0 +1,12 @@
+function nthTriangle(n){
+    ///////////////////////////////////////////////////////////////////////////////////////////*
+    // the formula for finding an nth triangle number is (n * n + 1) /2
+    // nth triangle number is the addition form of factorial i.e 1 + 2 + 3 + 4 is a nth triangle
+    // and !4 is 4 factorial
+    ///////////////////////////////////////////////////////////////////////////////////////////*
+}
+
+//Displays the result from nth triangle given N to pass into nth triangle by changing 
+function displayResult(){
+
+}
